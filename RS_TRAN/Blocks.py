ab15e1624eaa0727f4e31736afb6100118f4b5b4
QinglaiWeiCASIA <116715024+QinglaiWeiCASIA@users.noreply.github.com>
2022-10-28 12:41:50 +0800

    Add files via upload

---
+++ RS_TRAN/Blocks.py
@@ -0,0 +1,287 @@
+import torch
+from torch import einsum
+import torch.nn as nn
+from einops import rearrange
+
+from einops.layers.torch import Rearrange
+
+def conv1x1(in_channel, out_channel, stride=1):
+    return nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride, bias=False)
+
+
+def conv3x3(in_channel, out_channel, stride=1):
+    return nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None):
+        super(ResBlock, self).__init__()
+        self.conv1 = conv3x3(in_channel, out_channel, stride)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channel, out_channel)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+        if downsample is None:
+            self.downsample = nn.Identity()
+        else:
+            self.downsample = nn.Sequential(conv3x3(in_channel, out_channel, stride),nn.BatchNorm2d(out_channel))
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.downsample(x) + self.bn2(self.conv2(out)))
+
+        return out
+
+
+class ResBlock1x1(nn.Module):
+    def __init__(self, in_channel, out_channel, stride=1, downsample = None):
+        super(ResBlock1x1, self).__init__()
+        self.conv1 = conv1x1(in_channel, out_channel, stride)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv1x1(out_channel, out_channel)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+        
+        if downsample is None:
+            self.downsample = nn.Identity()
+        else:
+            self.downsample = nn.Sequential(conv3x3(in_channel, out_channel, stride),nn.BatchNorm2d(out_channel))
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.downsample(x) + self.bn2(self.conv2(out)))
+
+        return out
+
+
+
+class Reshape(nn.Module):
+    def __init__(self, shape):
+        super(Reshape, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.view(self.shape)
+    
+class Mean(nn.Module):
+    def __init__(self, dim, keepdim = False):
+        super(Mean, self).__init__()
+        self.dim = dim
+        self.keepdim = keepdim
+
+    def forward(self, x):
+        return x.mean(dim = self.dim, keepdim = self.keepdim)
+
+class Bottleneck(nn.Module):
+    def __init__(self,in_places,places, stride=1, downsampling=False, expansion = 4):
+        super(Bottleneck,self).__init__()
+        self.expansion = expansion
+        self.downsampling = downsampling
+
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(in_channels=in_places,out_channels=places,kernel_size=1,stride=1, bias=False),
+            nn.BatchNorm2d(places),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=places, out_channels=places, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(places),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=places, out_channels=places*self.expansion, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(places*self.expansion),
+        )
+
+        if self.downsampling:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels=in_places, out_channels=places*self.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(places*self.expansion)
+            )
+        self.relu = nn.ReLU(inplace=True)
+    def forward(self, x):
+        residual = x
+        out = self.bottleneck(x)
+        
+
+
+        if self.downsampling:
+            residual = self.downsample(x.contiguous())
+            
+
+
+        out += residual
+        out = self.relu(out)
+        return out
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+       
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        # hidden_dim = mlp_dim
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+# Attention
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 32, dropout = 0.):
+        #dim_head: dim of signle head
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+ 
+ 
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+ 
+ 
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+ 
+ 
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+ 
+ 
+    def forward(self, x, **kwargs):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+        attn = self.attend(dots)
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+    
+#%%   
+# 基于PreNorm、Attention和FFN搭建Transformer
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):#L
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x, name = 'yuanbeiming', name_didi = 'chendiancheng') + x
+            x = ff(x) + x
+        return x
+
+    
+class Clstransformer(nn.Module):
+    def __init__(self, words, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super(Clstransformer,self).__init__()
+        self.pos_embedding = nn.Parameter(torch.randn(1, words, dim))
+        # self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout = 0.)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        b,n,d = x.shape
+        x += self.pos_embedding[:, :]
+        # dropout
+        x = self.dropout(x)
+
+        x = self.transformer(x)
+
+        return x
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)       
+        
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size,  dim, depth, heads, mlp_dim, channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        #dim: lengh of token ，depth： depth of tranformer, dim_head: dim of signle head, mlp_dim: dim of mlp of transfomer
+        super().__init__()
+        image_height, image_width = image_size if isinstance(image_size, tuple) or  isinstance(image_size, list) else pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+     
+ 
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+        # patch数量
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        # patch维度
+        patch_dim = channels * patch_height * patch_width
+        
+        # 定义块嵌入
+        self.name = 'ViT'
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.Linear(patch_dim, dim),
+        )
+        # 定义位置编码
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches, dim))
+        # 定义类别向量
+
+        self.dropout = nn.Dropout(emb_dropout)
+ 
+ 
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+ 
+ 
+        self.to_latent = nn.Identity()
+        # 定义MLP
+
+    # ViT前向流程
+    def forward(self, img):
+        # 块嵌入
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+
+        # 追加位置编码
+        # print(x)
+        x += self.pos_embedding[:, :n]
+        # dropout
+        x = self.dropout(x)
+        # 输入到transformer
+        x = self.transformer(x)
+        # x_ = x.mean(dim = 1, keepdim = True) if self.pool == 'mean' else x[:,1:(n + 1)]
+        # MLP
+        return x        
+    
+class Bottleneck_judge(nn.Module):
+    def __init__(self, in_places, hidden_places, out_places = 1,  dropout = 0.1, last_dropout = 0.5):
+        super(Bottleneck_judge,self).__init__()
+
+
+
+        self.bottleneck = nn.Sequential(
+            nn.Linear(in_places, hidden_places),
+            nn.GELU(),
+            nn.BatchNorm1d(hidden_places),
+            nn.Linear(hidden_places, hidden_places),
+            nn.GELU(),
+            nn.BatchNorm1d(hidden_places),
+            nn.Linear(hidden_places, out_places)
+        )
+
+        if in_places != out_places:
+            self.downsample = nn.Sequential(
+                nn.Linear(in_places, out_places)
+            )
+            
+        else:
+            self.downsample = nn.Identity()
+            
+
+    def forward(self, x):
+
+        out = self.bottleneck(x)
+        
+        residual = self.downsample(x)
+        
+        out += residual
+        
+        return out
---
+++ RS_TRAN/ptransformer_raven.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 14 11:26:31 2021
+
+@author: yuanbeiming
+"""
+import torch
+import torch.nn as nn
+
+
+
+import torch.nn.functional as F
+
+
+if __name__ == '__main__':
+    from Blocks import *
+else:  
+    from .Blocks import *
+
+
+from einops.layers.torch import Rearrange
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class reasoning(nn.Module):
+    def __init__(self, args):
+        super(reasoning,self).__init__()
+
+        self.name = 'RS-Transformer_on_raven'
+
+        size = 80
+        patch = 20
+        
+        if args.big:
+            num_head = 8
+            num_depth = 6
+            self.low_dim = 256
+        else:
+            num_head = 4
+            num_depth = 3
+            self.low_dim = 128
+            
+        if args.dropout:
+            _dropout = 0.1
+        else:
+            _dropout = 0
+         
+            
+
+        self.w = int(size/patch)*int(size/patch)
+
+           
+        
+            
+        self.vit = nn.Sequential(ViT(image_size = size, 
+                                   patch_size = patch,  
+                                   dim = self.low_dim,
+                                   depth = num_depth, 
+                                   heads = num_head, 
+                                   mlp_dim = self.low_dim,
+                                   channels = 1, 
+                                   dim_head = int(self.low_dim/num_head), 
+                                   dropout = _dropout, 
+                                   emb_dropout = _dropout),
+                                 Rearrange('(b n) s d -> b n s d', n = 16)
+                                 )#b*16, 16, dim
+        
+        
+        
+        self.g_function = nn.Sequential(
+            Rearrange('b s w d -> (b s w) d'),
+
+            Bottleneck_judge(3*self.low_dim, int(self.low_dim*0.75), 3*self.low_dim),#10,10
+            
+            Bottleneck_judge(3*self.low_dim, int(self.low_dim*0.75), 3*self.low_dim),#10,10
+            
+            Bottleneck_judge(3*self.low_dim, int(self.low_dim*0.75), self.low_dim),#10,10
+            
+            Rearrange('(b s w) d -> b s w d', s = 10, w = self.w)
+
+            
+        )
+        
+
+        
+        self.high_law = nn.Sequential( #b ,8, 6, 16, 3, d
+                                        Rearrange('b s v w n d-> (b s v w) n d'),
+                                        Clstransformer(words = 3, dim = self.low_dim, depth = num_depth, heads = num_head, dim_head = int(self.low_dim/num_head), mlp_dim = self.low_dim, dropout = 0.1),
+                                        Rearrange('(b s v w) n d-> b s v w n d', s = 8, v = 1, w = self.w))
+ 
+        
+        
+        
+        
+        
+    
+        self.tajador = nn.Sequential(Rearrange('b s v w n d -> (b s v w n) d'),
+                            Bottleneck_judge(self.low_dim, self.low_dim),
+                            Rearrange('(b s v w n) d -> b s (v w n d)', s = 8, v = 1, w = self.w, n = 3),
+                            Mean(dim = 2))
+        
+        
+        
+        
+        
+    def cat(self, x):#b,16,...   b,10,...
+        
+        b, n, s, c = x.shape
+        #x  b, 16, 16 c
+        assert  n == 16
+        
+        r_law = [None]*10
+        
+        r_law[0] = torch.cat((x[:,0], x[:,1], x[:,2]), dim = -1)#b, 16, 3*d
+        r_law[1] = torch.cat((x[:,3], x[:,4], x[:,5]), dim = -1)#b, 16, 3*d
+        
+        
+        for i in range(8):
+    
+            r_law[i+2] = torch.cat((x[:,6], x[:,7], x[:,i+8]), dim = -1)#b, 16, 3*d
+            
+        return torch.stack(r_law, dim = 1) # b, 10, 16, 3*d
+    
+
+    
+    
+    def combine(self, r_law):
+        r_combine_0 = [None]*8
+      
+        assert r_law.shape[1] == 10 and r_law.shape[2] == self.w and r_law.shape[3] == self.low_dim
+            
+        for i in range(8):
+            r_combine_0[i] = torch.stack((r_law[:,0], r_law[:,1], r_law[:,2+i]), dim = 2)#b, 16 ,3 ,d, 
+           
+            
+        r_combine_0 = torch.stack(r_combine_0, dim = 1)#b ,8, 16, 3, d
+    
+        return r_combine_0.unsqueeze(2)
+    
+   
+    
+    
+    def forward(self, x):
+        b, n, h, w = x.shape
+        x = x.reshape(b*n, -1, h, w)
+        
+        x = self.vit(x)
+        
+        cat_row = self.cat(x)# b, 10,16 ,3*d
+        
+        r_law = self.g_function(cat_row)# b, 10, 16,d
+
+        rc_law = self.combine(r_law)#b, 8,  16, 3,d
+
+        high = self.high_law(rc_law)#b, 8,16, 6,d
+
+        return self.tajador(high).reshape(b,8), torch.empty(0).cuda()
+    
+
+    
+        
+    def choose(self, *out): 
+        
+        r = out[0]
+
+        return F.softmax(r, dim = -1)
+    
+
+    
+    
+    def loss_function(self, *out, target, temperature = 1):
+        
+        
+        r = out[0]
+
+        loss_1 = F.cross_entropy(r / temperature, target)
+
+        return loss_1
+        
+def transpose(x):
+    return x.transpose(-2, -1).contiguous()
+    
+def mul_dot(a, b):
+    
+    assert a.dim() == b.dim() and a.dim() == 3 and b.shape[1] == 1  and a.shape[1] == 8, 'dim error'
+    
+    # a@transpose(b)
+    
+    # print(a.shape,b.shape, (a@transpose(b)).shape)
+    return (a@transpose(b)).squeeze(-1)
+    
+    
+def normalize(*xs):
+    return [None if x is None else F.normalize(x, dim=-1) for x in xs]        
+
+ 
+if __name__ == '__main__':
+    from torchsummary  import summary
+    
+    model = reasoning().cuda()
+    
+    summary(model.cuda(), (16,80,80))
+    
